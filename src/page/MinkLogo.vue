c7cee719e92cfda48ec950fff7d13c9b4d6a24de
Sam Hardwick <sam.hardwick@csc.fi>
2026-04-30 09:24:22 +0300

    KP-11507 Delocalize to sloganless logo

--- src/page/MinkLogo.vue
+++ src/page/MinkLogo.vue
@@ -1,34 +1,15 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
 import logo from "@/assets/mink.svg";
-import logoSlogan from "@/assets/mink-slogan.svg";
-import logoSloganEn from "@/assets/mink-slogan-en.svg";
 import logoLight from "@/assets/mink-light.svg";
-import logoLightSlogan from "@/assets/mink-light-slogan.svg";
-import logoLightSloganEn from "@/assets/mink-light-slogan-en.svg";
 
-const props = defineProps<{
+defineProps<{
   large: boolean;
 }>();
-
-const { locale } = useI18n();
-
-const src = computed(() =>
-  props.large ? (locale.value == "en" ? logoSloganEn : logoSlogan) : logo,
-);
-const srcLight = computed(() =>
-  props.large
-    ? locale.value == "en"
-      ? logoLightSloganEn
-      : logoLightSlogan
-    : logoLight,
-);
 </script>
 
 <template>
   <picture>
-    <source media="(prefers-color-scheme: dark)" :srcset="srcLight" />
-    <img :src="src" :alt="$t('logo.alt')" :class="[large ? 'h-24' : 'h-16']" />
+    <source media="(prefers-color-scheme: dark)" :srcset="logoLight" />
+    <img :src="logo" :alt="$t('logo.alt')" :class="[large ? 'h-24' : 'h-16']" />
   </picture>
 </template>
